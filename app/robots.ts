83a0ebafa1c7f52041caaa5fa798f051b1e9ebb4
zhangyijie2026-afk <zhangyijie2026@gmail.com>
2026-08-23 18:04:31 -0700

    Restore latest gold Laicai logo

--- app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://laicaios.com/sitemap.xml",
-    host: "https://laicaios.com",
-  };
-}
